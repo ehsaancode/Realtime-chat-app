1ebf6dddac0167e93bbd73e4a91721ee8f5615a2
Ehsaan Mondal <mail.ehsaan@gmail.com>
2023-06-20 17:15:50 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,56 @@
+const socket = io()
+let name;
+let messageArea = document.querySelector('.message__area')
+
+do {
+    name = prompt("Enter your name:")
+} while (!name)
+
+let textarea = document.querySelector('#textarea')
+
+textarea.addEventListener('keyup', (e) => {
+    if (e.key === "Enter") {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+
+    //appending message
+
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    //send to server
+
+    socket.emit('message', msg)
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+//receiving message
+
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
